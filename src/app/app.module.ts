3bed52b4609efca3de051db2fefda42999f0b74e
Alex Calvo <alexcalvo402@gmail.com>
2024-02-06 00:00:53 +0100

    refactor carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IndexComponent } from './index/index.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
