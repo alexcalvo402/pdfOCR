38696061d5a7adf4394e6af603eed4b073d455b5
Alex Calvo <alexcalvo402@gmail.com>
2024-02-06 00:36:14 +0100

    gmaps service

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,9 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GoogleMapsService } from 'src/app/services/GoogleMaps.service';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss'],
 })
-export class IndexComponent { }
+export class IndexComponent { 
+
+constructor(private gmapsService: GoogleMapsService){
+}
+
+
+}
